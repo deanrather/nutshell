2b99e58f77160cb2966136cd0b8640df57fc6ffd
Timothy Chandler <tim.chandler@spinifexgroup.com>
2011-06-16 17:18:04 +1000

    Completed URL plugin.

--- plugin/url/URL.php
+++ plugin/url/URL.php
@@ -5,8 +5,10 @@ namespace nutshell\plugin\url
 	use nutshell\behaviour\Native;
 	use nutshell\behaviour\Singleton;
 	
-	class URL extends Plugin implements Native,Singleton
+	class Url extends Plugin implements Native,Singleton
 	{
+		private $nodes=array();
+		
 		public static function loadDependencies()
 		{
 			
@@ -14,7 +16,33 @@ namespace nutshell\plugin\url
 		
 		public function init()
 		{
-			
+			$nodes=explode('/',strtolower($_SERVER['REQUEST_URI']));
+			if (!end($nodes))	array_pop($nodes);
+			if (!reset($nodes))	array_shift($nodes);
+			if (!isset($nodes[0]))
+			{
+				$nodes[0]='';
+			}
+			$this->nodes=$nodes;
+		}
+		
+		public function node($num)
+		{
+			if (isset($this->node[$num]))
+			{
+				return $this->node[$num];
+			}
+			return null;
+		}
+		
+		public function nodeEmpty($num)
+		{
+			return (isset($this->nodes[$num]) && empty($this->nodes[$num]));
+		}
+		
+		public function getNodes()
+		{
+			return $this->nodes;
 		}
 	}
 }
