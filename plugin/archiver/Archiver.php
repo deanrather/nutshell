ad012ec4fa5fe725e02cee51d08712ab2751292b
Guillaume Bodi <guillaume@spinifexgroup.com>
2011-06-20 11:24:19 +1000

    Defined the Archiver API

--- plugin/archiver/Archiver.php
+++ plugin/archiver/Archiver.php
@@ -3,18 +3,29 @@ namespace nutshell\plugin\archiver
 {
 	use nutshell\core\plugin\Plugin;
 	use nutshell\behaviour\Native;
-	use nutshell\behaviour\Factory;
+	use nutshell\behaviour\AbstractFactory;
 	
-	class Archiver extends Plugin implements Native,Factory
+	class Archiver extends Plugin implements Native,AbstractFactory
 	{
 		public static function loadDependencies()
 		{
-			
+			include_once __DIR__ . '/engine/Base.php';
+			include_once __DIR__ . '/engine/Zip.php';
+			include_once __DIR__ . '/engine/Rar.php';
 		}
 		
-		public function init()
+		public static function runFactory($engine)
 		{
+			$className = __NAMESPACE__ . '\\engine\\' . ucfirst($engine);
 			
+			if (class_exists($className))
+			{
+				return new $className();
+			}
+			else
+			{
+				throw new ArchiverException(sprintf("Archiving engine not supported or misspelt: %s", $engine));
+			}
 		}
 	}
 }
---
+++ plugin/archiver/engine/Zip.php
@@ -0,0 +1,8 @@
+<?php
+namespace nutshell\plugin\archiver\engine 
+{
+	class Zip extends Base
+	{
+		
+	}
+}
