27e893ec83f056850de51832aeeff2be03b6bab1
Timothy Chandler <tim.chandler@spinifexgroup.com>
2011-06-23 17:57:45 +1000

    Added some shortcuts.

--- plugin/mvc/Controller.php
+++ plugin/mvc/Controller.php
@@ -1,15 +1,21 @@
 <?php
 namespace nutshell\plugin\mvc
 {
+	use nutshell\Nutshell;
+
 	abstract class Controller
 	{
-		public $MVC	=null;
-		public $view=null;
+		public $core	=null;
+		public $plugin	=null;
+		public $MVC		=null;
+		public $view	=null;
 		
 		public function __construct(Mvc $MVC)
 		{
-			$this->MVC	=$MVC;
-			$this->view	=new View($this->MVC);
+			$this->core		=Nutshell::getInstance();
+			$this->plugin	=$this->core->plugin;
+			$this->MVC		=$MVC;
+			$this->view		=new View($this->MVC);
 		}
 	}
 }
