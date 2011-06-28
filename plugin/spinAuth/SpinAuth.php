3f31e04549587cf68362e58878e9ede433de94f2
Timothy Chandler <tim.chandler@spinifexgroup.com>
2011-06-28 11:51:37 +1000

    New spinAuth module. Contains dummy code for now.

---
+++ plugin/spinAuth/SpinAuth.php
@@ -0,0 +1,42 @@
+<?php
+namespace nutshell\plugin\spinAuth
+{
+	use nutshell\Nutshell;
+	use nutshell\core\plugin\Plugin;
+	use nutshell\behaviour\Native;
+	use nutshell\behaviour\Singleton;
+	use nutshell\behaviour\session\NamedSession;
+	use \stdClass;
+	
+	class SpinAuth extends Plugin implements Native,Singleton,NamedSession
+	{
+		public static function loadDependencies()
+		{
+			
+		}
+		
+		public static function registerBehaviours()
+		{
+			
+		}
+		
+		public function init()
+		{
+			
+		}
+		
+		public function login($username,$password)
+		{
+			//Dummy data!
+			$response				=new stdClass();
+			$response->success		=true;
+			$response->code			=1;
+			$response->token		='foo12345';
+			
+			//Save the token in the session and return the response.
+			$this->session->token	=$response->token;
+			return $response;
+		}
+	}
+}
+?>
