2ffcdca55f8ba7680365acba3ddcc15caa5ae702
Joao Schuler <joao.schuler@spinifexgroup.com>
2011-08-17 15:43:27 +1000

    Adding Model Generator. It can be used to create php models from tables in a database.

---
+++ plugin/modelGenerator/ModelGenerator.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * ModelGenerator is used to generate table models.
+ * @package nutshell-plugin
+ * @author joao
+ */
+namespace nutshell\plugin\modelGenerator
+{
+	use nutshell\core\plugin\Plugin;
+	use nutshell\behaviour\Native;
+	use nutshell\behaviour\Singleton;
+	use nutshell\Nutshell;
+
+	/**
+	 * @author joao
+	 * @package nutshell-plugin
+	 */
+	class ModelGenerator extends Plugin implements Native,Singleton
+	{
+		protected $db;
+		
+		public static function loadDependencies()
+		{
+			/* if(!self::$dependenciesLoaded)
+			{
+				//include_once(__DIR__.'../plugin/impl/DB.php');
+			}
+			*/
+		}
+		
+		public static function registerBehaviours()
+		{
+			
+		}
+		
+		public function init()
+		{
+			if ($connection=Nutshell::getInstance()->config->plugin->Mvc->connection)
+			{
+				$this->db=Nutshell::getInstance()->plugin->Db->{$connection};
+			} else {
+				throw new Exception('Model generator can\'t find a db connection.');
+			} 
+		}
+		
+		/**
+		 * This function returns a PHP code containing an array with columns that compose the primary key.
+ 		 * @param array $tableStructure
+		 * @return string
+		 */
+		protected function getPKArray(&$tableStructure)
+		{
+			$PKs = array();
+			foreach($tableStructure as &$column)
+			{
+				if ($column['COLUMN_KEY']=='PRI')
+				{
+					$PKs[] = "'".$column['COLUMN_NAME']."'";
+				}
+			}
+			return 'array('.implode(',',$PKs).')';
+		}
+		
+		/**
+		 * This function returns a PHP code containing an array with column definition.
+ 		 * @param array $tableStructure
+		 * @return string
+		 */
+		protected function getColumnDefinition(&$tableStructure)
+		{
+			$rows = array();
+			foreach($tableStructure as &$column)
+			{
+				$column_name = $column['COLUMN_NAME'];
+				$column_type = $column['COLUMN_TYPE'];
+				
+				if ($column['IS_NULLABLE']=='NO')
+				{
+					$column_type = $column_type.' NOT NULL ';
+				}
+				
+				$rows[] = "				'$column_name' => '$column_type' ";
+			}
+
+			return "array(\n".implode(",\n", $rows).')';
+		}
+
+		/**
+		* This function indicates if auto increment is used.
+		* @param array $tableStructure
+		* @return boolean
+		*/
+		protected function isAutoIncrement(&$tableStructure)
+		{
+			$result = false;
+			foreach($tableStructure as &$column)
+			{
+				$result = $result || ($column['EXTRA']=='auto_increment');
+			}
+			return $result;
+		}
+		
+		/**
+		 * This function returns a PHP code containing a full database model code for a given table.
+ 		 * @param array $table_name  Name of the table in the database.
+ 		 * @param array $folder      Folder inside the "model" folder. In many cases this will be an empty string. In the case you provide a folder name, it has to end with a '\\'.
+ 		 * @param array $model_name  This is the class name
+ 		 * @param array $autoCreate  Indicates if tables should be created when missing. In most cases this value should be false.
+		 * @return string
+		 */
+		public function getModelStrFromTable($table_name, $folder, $model_name, $autoCreate = false)
+		{
+			$tableStructure    = $this->db->getColumnsFromMysqlTable($table_name);
+			$pk_array_str      = $this->getPKArray($tableStructure);
+			$primary_ai_str    = $this->isAutoIncrement($tableStructure) ? "true" : "false";
+			$autoCreate_str    = $autoCreate ? "true" : "false";
+			$column_definition = $this->getColumnDefinition($tableStructure);
+			
+			$code = 
+"<?php
+namespace application\model\\$folder
+{
+	use nutshell\plugin\mvc\model\CRUD;
+	
+	class $model_name extends CRUD
+	{
+		public \$name       ='$table_name';
+		public \$primary    =$pk_array_str;
+		public \$primary_ai =$primary_ai_str;
+		public \$autoCreate =$autoCreate_str;
+		
+		public \$columns    =$column_definition;
+	}
+}
+?>";
+			return $code;
+		}
+		
+	}// class
+}// namespace
