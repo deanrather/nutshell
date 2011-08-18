c68df32f27d994dbb51a85d82357c9059a623ebc
Joao Schuler <joao.schuler@spinifexgroup.com>
2011-08-18 14:21:56 +1000

    Updating documentation because of updated CRUD.

--- _examples/MVCHelloWorld/private/application/model/account(example).php
+++ _examples/MVCHelloWorld/private/application/model/account(example).php
@@ -5,8 +5,10 @@ namespace application\model
 	
 	class Account extends CRUD
 	{
-		public $name	='account';
-		public $primary	='id';
+		public $name	   ='account';   // This is the table name. 
+		public $primary	   =array('id'); // All columns that compose the PK should be listed here.
+		public $primary_ai =true;        // Default value is true. Indicates that the primary key is "auto increment".
+		public $autoCreate =true;        // Default value is true. Indicates that the table has to be created when it is not found in the database.
 		
 		public $columns=array
 		(
