5a6b88f67cf6bc05fa758fe36de2abf255c79adb
Joao Schuler <joao.schuler@spinifexgroup.com>
2011-08-24 11:19:09 +1000

    Allows a model to have access to nutshell, plugins and other models.

--- plugin/mvc/Model.php
+++ plugin/mvc/Model.php
@@ -7,6 +7,7 @@ namespace nutshell\plugin\mvc
 {
 	use nutshell\core\plugin\PluginExtension;
 	use nutshell\behaviour\Loadable;
+	use nutshell\Nutshell;
 	
 	/**
 	 * @author guillaume
@@ -15,9 +16,33 @@ namespace nutshell\plugin\mvc
 	 */
 	abstract class Model implements Loadable
 	{
+		/**
+		 * Stores a local pointer to Nutshell
+		 */ 
+		public $nutshell = null;
+		
+		/**
+		 * Stores a local pointer to plugin loader. 
+		 */
+		public $plugin	 = null;
+		
+		/**
+		 * Stores a local pointer to model loader.
+		 */
+		public $model    = null;
+		
 		public function __construct()
 		{
-			
+			$this->nutshell = Nutshell::getInstance();
+			if (isset($this->nutshell))
+			{
+				$this->plugin = $this->nutshell->plugin;
+				$loader=$this->nutshell->getLoader();
+				if (isset($loader))
+				{
+					$this->model = $loader('model');
+				}
+			}
 		}
 		
 		public static function getInstance(Array $args=array())
