19777ebbf8147ac62023c9d8ec3b7dce7f3a973d
Joao Schuler <joao.schuler@spinifexgroup.com>
2011-08-29 15:06:22 +1000

    2 new methods to the array helper:
    renameColumn(&$data, $old_column_name, $new_column_name);
    createKeyEqualsValueArray($aArrayToBeTransformed);

--- helper/ArrayClass.php
+++ helper/ArrayClass.php
@@ -11,14 +11,13 @@ namespace nutshell\helper
 	 * This class has specialized methods for dealing 
 	 * with arrays.
 	 *  
-	 * @author guillaume
 	 * @package nutshell
 	 * @static
 	 */
 	class ArrayClass
 	{
 		/**
-		 * This function adds a column to a 2 dimensioinal array.
+		 * This function adds a column to a 2 dimensional array.
 		 * @param array $data
 		 * @param string $column_name
 		 * @param mixed $value
@@ -29,6 +28,38 @@ namespace nutshell\helper
 			{
 				$row[$column_name] = $value;
 			}
+		}
+
+		/**
+		 * This function renames a column in a 2 dimensional array.
+		 * @param array
+		 * @param string $old_column_name
+		 * @param string $new_column_name
+		 */
+		public static function renameColumn(&$data, $old_column_name, $new_column_name)
+		{
+			if (($old_column_name != $new_column_name) && (strlen($old_column_name)>0) && (strlen($new_column_name)>0) )
+			{
+				foreach($data as &$row)
+				{
+					$row[$new_column_name] = $row[$old_column_name];
+					unset($row[$old_column_name]);
+				}
+			}
+		}
+		
+		/**
+		 * This function returns an array where the $key is the same 
+		 * @param array $aArrayToBeTransformed
+		 */
+		public static function createKeyEqualsValueArray($aArrayToBeTransformed)
+		{
+			$result = array ();
+			foreach($aArrayToBeTransformed as $value)
+			{
+				$result[$value] = $value;
+			}
+			return $result;
 		}
 	}
 }
