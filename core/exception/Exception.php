84fba1d378e078785f10bd95924aa9c69cf1b6d2
Joao Schuler <joao.schuler@spinifexgroup.com>
2011-09-02 12:11:36 +1000

    Now errors and exceptions are logged.

--- core/exception/Exception.php
+++ core/exception/Exception.php
@@ -2,55 +2,161 @@
 /**
  * @package nutshell
  */
-namespace nutshell\core\exception
+namespace
 {
-	use nutshell\core\Component;
 	use nutshell\Nutshell;
-	use \Exception as SPLException;
-	
-	/**
-	 * @package nutshell
-	 */
-	class Exception extends SPLException
+
+	class NutshellException extends Exception
 	{
+		
 		/**
-		 * Prevents recursion.
-		 * @var bool
-		 */
+		* Prevents recursion.
+		* @var bool
+		*/
 		private static $blockRecursion = false;
 		
-		// logs the error message
-		public function __construct ($message, $code = null, Exception $previous = null)
+		
+		/**
+		 * Error handler used before the class is created.
+		 */
+		private static $oldErrorHandler = false;
+		
+		/**
+		 * Indicates that errors should be shown. (depends on ns_env environment variable).
+		 * @var bool
+		 */
+		private static $echoErrors = false;
+		
+		/**
+		 * All errors that shouldn't be shown in the user interface.
+		 * @param unknown_type $message
+		 */
+		private static $dontShowErrors = array
+		(
+			2048 => 1
+		);
+
+		/**
+		 * Echoes an error if $echoErrors and not $dontShowErrors[$errno]
+		 * @param int $errno
+		 * @param string $message
+		 */
+		private static function echoError($errno, $message)
+		{
+			if (self::$echoErrors) 
+			{
+				if (!isset(self::$dontShowErrors[$errno]))
+				{
+					echo $message;
+				}
+			}
+		}
+		
+		/**
+		 * Logs a message if Nutshell has a loader.
+		 * @param string $message
+		 */
+		private static function logMessage($message)
+		{
+			if (strlen($message)>0)
+			{
+				$nutInst = Nutshell::getInstance();
+				if ($nutInst->hasLoader())
+				{
+					$log = $nutInst->plugin->Logger();
+					$log->fatal($message);
+				}
+			}			
+		}
+		
+		/**
+		 * This method treats (and logs) errors.
+		 * @param int $errno
+		 * @param string $errstr
+		 * @param string $errfile
+		 * @param int    $errline
+		 * @param array $errcontext
+		 */
+		public static function treatError($errno, $errstr = null, $errfile = null, $errline = null, array $errcontext = null)
 		{
 			if (!self::$blockRecursion)
 			{
 				self::$blockRecursion = true;
-				
+			
 				try // to log
 				{
-					$message  = (strlen($message)>0) ? "error message: $message" : "" ;
-					$message .= ($code > 0) ? "error code: $code" : "" ;
-					if (strlen($message)>0)
-					{
-						$nutInst = Nutshell::getInstance();
-						if ($nutInst->hasLoader())
-						{
-							$log = $nutInst->plugin->Logger();
-							$log->fatal("error message: $message, error code: $code");
-						}
-					}
-				} catch (Exception $e) {
+					$message =
+						"ERROR $errno. ".
+						( (strlen($errstr)>0)  ? "Message: $errstr. " : "").
+						( (strlen($errfile)>0) ? "File: $errfile. " : "").
+						( ($errline>0) ? "Line: $errline. " : "") ;
+						self::echoError($errno, $message);
+					self::logMessage($message);		
+				} catch (Exception $e) 
+				{
 					// nothing can be done if log fails here.
 				}
+				self::$blockRecursion = false;
 			}
-			
-			self::$blockRecursion = false;
-			parent::__construct($message, $code, $previous);
+			return false;
 		}
 		
+		/**
+		 * This method is called when an exception happens.
+		 * @param Exception $exception
+		 */
+		public static function treatException($exception)
+		{
+			if (!self::$blockRecursion)
+			{
+				self::$blockRecursion = true;
+			
+				try // to log
+				{
+					$message =
+						( ($exception->code>0)             ? "ERROR {$exception->code}. " : "" ).
+						( "Exception class:".get_class($exception).". ").
+						( (strlen($exception->message)>0)  ? "Message: {$exception->message}. " : "").
+						( (strlen($exception->file)>0)     ? "File: {$exception->file}. " : "").
+						( ($exception->line>0)             ? "Line: {$exception->line}. " : "") ;
+						
+					if (self::$echoErrors) 
+					{
+						echo $message;
+					}
+					self::logMessage($message);		
+				} catch (Exception $e) 
+				{
+					// nothing can be done if log fails here.
+				}
+				self::$blockRecursion = false;
+			}
+		}
+		
+		public static function setHandlers()
+		{
+			set_exception_handler('NutshellException::treatException');
+			self::$oldErrorHandler = set_error_handler('NutshellException::treatError');
+			self::$echoErrors = ((getenv('NS_ENV')=='dev') || (apache_getenv("NS_ENV") == 'dev'));
+		}
+	}
+
+	NutshellException::setHandlers();
+}
+
+namespace nutshell\core\exception
+{
+	use nutshell\core\Component;
+	use nutshell\Nutshell;
+	
+	/**
+	 * @package nutshell
+	 */
+	class Exception extends \NutshellException
+	{			
 		public static function register() 
 		{
 			Component::load(array());
-		}		
-	}
-}
+		}
+	} // end of class	
+} // end of namespace
