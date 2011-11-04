e5fc47ce0f6c9fab477002c456592fe291d81588
Joao Schuler <joao.schuler@spinifexgroup.com>
2011-11-04 12:54:42 +1100

    ArrayHelper: minor changes to support the future largeQueryPlugin.

--- helper/ArrayHelper.php
+++ helper/ArrayHelper.php
@@ -29,6 +29,31 @@ namespace nutshell\helper
 				$row[$column_name] = $value;
 			}
 		}
+		
+		/**
+		 * This function renames a field in a 1 dimensional array.
+		 * @param array  $row
+		 * @param string $old_column_name
+		 * @param string $new_column_name
+		 */		
+		public function renameField(array &$row, $old_field_name, $new_field_name)
+		{
+			$row[$new_field_name] = $row[$old_field_name];
+			unset($row[$old_field_name]);
+		}
+		
+		/**
+		 * This function renames a multiple fields in a 1 dimensional array.
+		 * @param array $row Data to be modified.
+		 * @param array $aRenaming Array with multiple renamings. This parameter is passed as reference because it's faster. This array won't be modified.
+		 */
+		public function renameFields(array &$row, array &$aRenaming)
+		{
+			foreach($aRenaming as $old_field_name => $new_field_name)
+			{
+				self::renameField($row, $old_field_name, $new_field_name);
+			}
+		}
 
 		/**
 		 * This function renames a column in a 2 dimensional array.
@@ -36,14 +61,13 @@ namespace nutshell\helper
 		 * @param string $old_column_name
 		 * @param string $new_column_name
 		 */
-		public static function renameColumn(&$data, $old_column_name, $new_column_name)
+		public static function renameColumn(array &$data, $old_column_name, $new_column_name)
 		{
 			if (($old_column_name != $new_column_name) && (strlen($old_column_name)>0) && (strlen($new_column_name)>0) )
 			{
 				foreach($data as &$row)
 				{
-					$row[$new_column_name] = $row[$old_column_name];
-					unset($row[$old_column_name]);
+					self::renameField($row, $old_column_name, $new_column_name);
 				}
 			}
 		}
@@ -52,7 +76,7 @@ namespace nutshell\helper
 		 * This function returns an array where the $key is the same 
 		 * @param array $aArrayToBeTransformed
 		 */
-		public static function createKeyEqualsValueArray($aArrayToBeTransformed)
+		public static function createKeyEqualsValueArray(array $aArrayToBeTransformed)
 		{
 			$result = array ();
 			foreach($aArrayToBeTransformed as $value)
