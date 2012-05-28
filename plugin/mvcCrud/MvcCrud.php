ba559561617ef613934d73493b6a7cfeebfc597d
Timothy Chandler <tim.chandler@spinifexgroup.com>
2012-05-28 11:42:18 +1000

    Updated plugins and CRUD class.

---
+++ plugin/mvcCrud/MvcCrud.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package nutshell-plugin
+ * @author Timothy Chandler <tim.chandler@spinifexgroup.com>
+ */
+namespace nutshell\plugin\mvcCrud
+{
+	use nutshell\core\plugin\LibraryPlugin;
+	use nutshell\core\exception\NutshellException;
+	
+	/**
+	 * @author Timothy Chandler <tim.chandler@spinifexgroup.com>
+	 * @package nutshell-plugin
+	 */
+	class MvcCrud extends LibraryPlugin
+	{
+		public function init()
+		{
+			$this->plugin->Mvc;
+		}
+	}
+}
+?>
